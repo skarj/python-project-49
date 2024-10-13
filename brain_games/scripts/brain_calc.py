c88746f9a3f4951967a2a4f7cd7fcd807aa713a4
skarj <sergey.baranov@friendsurance.de>
2024-10-13 22:45:54 +0300

    Change algoritm of prime check

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.engine.game import start_game
-from brain_games.games import calc
+from brain_games.games.calc import play
 
 
 def main():
-    start_game(calc)
+    play()
 
 
 if __name__ == '__main__':
