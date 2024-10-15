bc93859a8c1f8bfac7b7d50e164988747fe7189f
skarj <sergey.baranov@friendsurance.de>
2024-10-16 00:15:37 +0300

    Improve prime check function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,15 +5,15 @@ from math import sqrt
 def is_number_prime(number):
     prime_flag = 0
 
-    if number > 1:
-        for i in range(2, int(sqrt(number)) + 1):
-            if number % i == 0:
-                prime_flag = 1
-                break
-        if prime_flag == 0:
-            return True
-        else:
-            return False
+    if number <= 1:
+        return False
+
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            prime_flag = 1
+            break
+    if prime_flag == 0:
+        return True
     else:
         return False
 
