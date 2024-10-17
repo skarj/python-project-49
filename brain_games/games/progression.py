04a5f8d6d0512efb6d78d145e0813d593b7e4828
skarj <sergey.baranov@friendsurance.de>
2024-10-17 18:13:33 +0300

    Fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,6 +3,7 @@ from random import randint
 DESCRIPTION = 'What number is missing in the progression?'
 PROGRESSION_LENGTH = 10
 
+
 def get_question_and_answer():
     step = randint(1, 5)
     start = randint(1, 15)
