053ec568de430d4915b284f31194abdf08694365
skarj <sergey.baranov@friendsurance.de>
2024-10-17 18:12:00 +0300

    Fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,7 +6,7 @@ PROGRESSION_LENGTH = 10
 def get_question_and_answer():
     step = randint(1, 5)
     start = randint(1, 15)
-    index = randint(0, PROGRESSION_LENGTH)
+    index = randint(0, PROGRESSION_LENGTH - 1)
 
     sequence = [str(i) for i in range(start, start + step * PROGRESSION_LENGTH, step)]
     answer = sequence[index]
